3ea3c558942dada381f1276042c756f592759396
Marko Anastasov <marko.anastasov@gmail.com>
2008-08-26 13:32:08 +0200

    Adding testing capabilities with boost-testing.

--- src/main.cc
+++ src/main.cc
@@ -1,23 +1,9 @@
 
-#include "feature-ex.hh"
 #include <iostream>
-#include <vector>
-#include <boost/function.hpp>
-#include <glibmm/convert.h>
 
 int
 main(int /*argc*/, char** /*argv*/)
 {
-    std::vector<Glib::ustring> results;
-    wordtip::split_simple("хеј man!\n have a cigar();", results);
-    std::vector<Glib::ustring>::iterator it(results.begin());
-    std::vector<Glib::ustring>::iterator end(results.end());
-    try {
-        for ( ; it != end; ++it)
-            std::cout << it->raw() << std::endl;
-    } catch (const Glib::ConvertError& ex) {
-        std::cerr << ex.what() << std::endl;
-    }
-
+    std::cout << "Running...?" << std::endl;
     return 0;
 }
---
+++ tests/test-split-simple.cc
@@ -0,0 +1,34 @@
+
+#include <boost/test/unit_test.hpp>
+#include "feature-ex.hh"
+
+void
+test_construct()
+{
+    std::vector<Glib::ustring> results;
+    wordtip::split_simple("жзхеј man!\n have a... cigar();", results);
+    BOOST_CHECK_EQUAL(results.size(), 4);
+
+    Glib::ustring item;
+    item = results[0];
+    BOOST_CHECK_EQUAL(item.size(), 5);
+
+    item = results[1];
+    BOOST_CHECK_EQUAL(item.size(), 3);
+    
+    item = results[2];
+    BOOST_CHECK_EQUAL(item.size(), 4);
+    
+    item = results[3];
+    BOOST_CHECK_EQUAL(item.size(), 5);
+}
+
+using namespace boost::unit_test;
+
+test_suite*
+init_unit_test_suite(int,char**)
+{
+  test_suite* test = BOOST_TEST_SUITE("split-simple");
+  test->add(BOOST_TEST_CASE(&test_construct));
+  return test;
+}
